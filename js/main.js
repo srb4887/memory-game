ca54dbc4db1023e6ac091e7f9bd0f14a30efa1a7
Stephen Bowers <srb4887@gmail.com>
2018-05-29 19:58:07 -0700

    feat: Add hover effect and selected class

--- js/main.js
+++ js/main.js
@@ -2,14 +2,18 @@
 const board = document.getElementById('board');
 const cards = document.getElementsByClassName('card');
 const cardBacks = document.getElementsByClassName('card-back');
-
-// TO DO: Make card borders highlighted when hovered on
-
-board.addEventListener('mouseover', function(){
+const cardFronts = document.getElementsByClassName('card-front');
 
     for(let cardBack of cardBacks) {
+        cardBack.addEventListener('mouseenter', function(event) {
+            event.target.classList.add('hover');
+        });
+        cardBack.addEventListener('mouseleave', function(event) {
+            event.target.classList.remove('hover');
+        });
         cardBack.addEventListener('click', function(event) {
             event.target.style.zIndex = '-2';
+            event.target.previousElementSibling.classList.add('selected');
         });
     }
-})
+
